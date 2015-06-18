e285308de35a5221e8c1cd71589904548017b0ca
Jan Daniel Andersen <jand187@gmail.com>
2015-06-18 12:06:31 +0200

    Added DownloadSpec.

---
+++ GrabItems/DownloadSpec.cs
@@ -0,0 +1,16 @@
+namespace GrabItems
+{
+	internal class DownloadSpec
+	{
+		public DownloadSpec(int index)
+		{
+			Index = index;
+		}
+
+		public int Index
+		{
+			get;
+			private set;
+		}
+	}
+}
