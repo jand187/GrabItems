c7ccf613607f421da7778576abbb14471956835c
Jan Daniel Andersen <jand187@gmail.com>
2015-06-18 12:08:35 +0200

    Added more models.

---
+++ GrabItems/Error.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace GrabItems
+{
+	internal class Error
+	{
+		[JsonProperty("message")]
+		public string Message
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ GrabItems/TabModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace GrabItems
+{
+	internal class TabModel
+	{
+		[JsonProperty("numTabs", DefaultValueHandling = DefaultValueHandling.Ignore)]
+		public int NumberOfTabs
+		{
+			get;
+			set;
+		}
+
+		[JsonProperty("error")]
+		public Error Error
+		{
+			get;
+			set;
+		}
+	}
+}
